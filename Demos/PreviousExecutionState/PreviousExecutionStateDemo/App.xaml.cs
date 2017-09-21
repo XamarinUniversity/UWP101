9a435224a499b775eb41d6360bfdc0cad41bd328
Adam Patridge <patridge@gmail.com>
2017-09-21 14:53:01 -0600

    Add files missed by migration.

---
+++ Demos/PreviousExecutionState/PreviousExecutionStateDemo/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Windows.ApplicationModel.Activation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace PreviousExecutionStateDemo
+{
+	sealed partial class App : Application
+    {
+        public App()
+        {
+            this.InitializeComponent();
+        }
+
+		protected override void OnLaunched(LaunchActivatedEventArgs e)
+		{
+			// NotRunning
+			// ClosedByUser
+			// Running
+			// Suspended
+			// Terminated
+			string state = e.PreviousExecutionState.ToString();
+
+			Window.Current.Content = new TextBlock() { Text = state, VerticalAlignment=VerticalAlignment.Center, HorizontalAlignment = HorizontalAlignment.Center };
+
+			Window.Current.Activate();
+		}
+	}
+}
